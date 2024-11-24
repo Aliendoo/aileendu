02243fab68c44bd6cbc4db0cd141c2573a48fcbd
AileenDu <89880384+Aliendoo@users.noreply.github.com>
2024-11-23 23:56:37 -0500

    Add files via upload

---
+++ assets/js/contact.js
@@ -0,0 +1,28 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_uqon1it', 'template_jgvu0zk', '#contact-form', '1CoN24AewDs7DHR_C')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
